8fb7d691e80d15cbad17ea22684fc6e7d9ba049a
Konstantine Kosykh <2k@MacBook-Air-Konstantin.local>
2023-11-04 20:18:53 +0600

    ЕЕЕЕЕЕЕЕЕЕ

--- src/Calc/Main.java
+++ src/Calc/Main.java
@@ -1,47 +1,62 @@
-// В общем да,  задача не  стандартная  по  сути на знание множества методов и "архитектуры" (может  я не  верно выражаюсь).
-// На самом деле  пришлось повозиться и да, я не  верно  изначально воспринял  условия задачи, прошу прощения.
-// Создал  клас Main со  сканером строки, отправил все в  метод Calc, где в свою  очередь осуществлен парсинг строки,
-// как было написанов задании (принимует  input - как целуб  строку а не  разбитые  элементы),  пришлось повозиться.
-// В  остальном ничего особо не  менял,  убрал  лишние выводы (так как  они  теперь не нужны, оставил  првоерки на
-// корректный  ввод).
-
 
 package Calc;
-
 import java.util.Scanner;
 class Main {
+
     public static void main(String[] args) {
         System.out.println("Привет введите выражение для вычисления от 1 до 10  включительно");
         Scanner scan = new Scanner(System.in);
         String input = scan.nextLine();
         System.out.println("Результат: " + calc(input));
     }
-    public static String calc (String input) {
+
+    public static int calc(String input) {
         String[] elements = input.split(" ");
-        int a = Integer.parseInt(elements[0]);
-        String oper = elements[1];
-        int b = Integer.parseInt(elements[2]);
-        int res = 0;
-        if (a > 10 | a < 1 | b > 10 | b < 1) {
-            System.out.println("Вы ввели число вне диапазона");
+        int result = 0;
+        if (elements.length != 3) //проверка на количество элементов,  среди которых знак операыии
+            System.out.println("Exception: Некорректная длинна примера  ");
+        else {
+            int num1 = numBers(elements[0]);
+            int num2 = numBers(elements[2]);
+            String oper = elements[1];
+            result = vichMat(num1, num2, oper);
+        }
+        return result;
+    }
+
+    static int numBers(String s) {
+        int num = 0;
+        try {
+            num = Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            System.out.print("Exception: ");
+        }
+        return num;
+    }
+    static int vichMat(int a, int b, String znak) {
+        int oper = 0;
+        if (a > 10 || a < 1 || b > 10 || b < 1) {   // проверка на  диапазон чисел
+            System.out.println("Некорректное число или число вне диапазона");
         } else {
-            switch (oper) {
+            switch (znak) {
                 case "+":
-                    res = a + b;
+                    oper = a + b;
                     break;
                 case "-":
-                    res = a - b;
+                    oper = a - b;
                     break;
                 case "*":
-                    res = a * b;
+                    oper = a * b;
                     break;
                 case "/":
-                    res = a / b;
+                    oper = a / b;
                     break;
                 default:
+
                     System.out.println("Exception ввели недопустимое действие");
             }
         }
-        return Integer.toString(res);
+        return oper;
     }
 }
+
